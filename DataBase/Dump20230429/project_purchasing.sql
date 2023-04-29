ae3299033e33e390ca993843c6cc806fccf1daba
Nguyễn Quang Huy <nqhuy2092004@gmail.com>
2023-04-29 21:59:49 +0700

    add DB

---
+++ DataBase/Dump20230429/project_purchasing.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: project
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `purchasing`
+--
+
+DROP TABLE IF EXISTS `purchasing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchasing` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `shipment_id` int NOT NULL,
+  `order_details_id` int NOT NULL,
+  `discount_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `purchasing_ibfk_1` (`discount_id`),
+  KEY `purchasing_ibfk_2` (`order_details_id`),
+  KEY `purchasing_ibfk_3` (`shipment_id`),
+  CONSTRAINT `purchasing_ibfk_1` FOREIGN KEY (`discount_id`) REFERENCES `discount` (`id`),
+  CONSTRAINT `purchasing_ibfk_2` FOREIGN KEY (`order_details_id`) REFERENCES `order_details` (`id`),
+  CONSTRAINT `purchasing_ibfk_3` FOREIGN KEY (`shipment_id`) REFERENCES `shipment` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchasing`
+--
+
+LOCK TABLES `purchasing` WRITE;
+/*!40000 ALTER TABLE `purchasing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchasing` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-29 14:56:04
